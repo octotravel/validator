d6b31f60ad97410c4056a848637880bcc2ea2dd2
Dominik Chrastek <dominik.chrastek@gmail.com>
2022-12-19 13:39:08 +0100

    add check for no availability returned

--- server/src/services/validation/helpers/AvailabilityScenarioHelper.ts
+++ server/src/services/validation/helpers/AvailabilityScenarioHelper.ts
@@ -1,3 +1,5 @@
+import { ErrorType, ValidatorError } from './../../../validators/backendValidator/ValidatorHelpers.ts';
+import * as R from "https://esm.sh/ramda@0.28.0";
 import { Availability, Product } from "https://esm.sh/@octocloud/types@1.3.1";
 import { AvailabilityValidator } from "../../../validators/backendValidator/Availability/AvailabilityValidator.ts";
 import { ScenarioHelper, ScenarioHelperData } from "./ScenarioHelper.ts";
@@ -22,6 +24,13 @@ export class AvailabilityScenarioHelper extends ScenarioHelper {
     const availabilities = result?.data ?? [];
 
     const errors = availabilities.map(validator.validate).flat();
+
+    if (R.isEmpty(availabilities)) {
+      errors.push(new ValidatorError({
+        type: ErrorType.CRITICAL,
+        message: `no availability returned`,
+      }))
+    }
 
     return this.handleResult({
       ...data,
