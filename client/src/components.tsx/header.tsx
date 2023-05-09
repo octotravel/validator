6ece694786465a72d81327a3e3b90b163f3b8c75
Michal Panský <pansky.michal@gmail.com>
2023-05-09 16:22:05 +0200

    add meta tag

--- client/src/components.tsx/header.tsx
+++ client/src/components.tsx/header.tsx
@@ -11,11 +11,14 @@ const Header: FC<IHeader> = ({ title }) => {
     document.title = 'OCTO Validation Tool';
   }, []);
   return (
-    <div className="page-header">
-      <Container>
-        <h2 className=" mt-3 mb-3 page-title">{title}</h2>
-      </Container>
-    </div>
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <div className="page-header">
+        <Container>
+          <h2 className=" mt-3 mb-3 page-title">{title}</h2>
+        </Container>
+      </div>
+    </>
   )
 }
 
