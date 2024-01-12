b2e3d326d3cb349b55a3295d5dd0bbebca524e24
Michal Panský <pansky.michal@gmail.com>
2024-01-12 13:45:25 +0100

    add pricing capability

--- server/src/services/validation/Scenarios/Capabilities/GetCapabilities.ts
+++ server/src/services/validation/Scenarios/Capabilities/GetCapabilities.ts
@@ -2,6 +2,7 @@ import { Scenario } from "../Scenario";
 import { CapabilitiesScenarioHelper } from "../../helpers/CapabilitiesScenarioHelper";
 import descriptions from "../../consts/descriptions";
 import { Context } from "../../context/Context";
+import { CapabilityId } from "@octocloud/types";
 
 export class GetCapabilitiesScenario implements Scenario {
   private capabilitiesScenarioHelper = new CapabilitiesScenarioHelper();
@@ -12,6 +13,10 @@ export class GetCapabilitiesScenario implements Scenario {
     const result = await apiClient.getCapabilities(context);
     const name = "Get Capabilities";
     const description = descriptions.getCapabilities;
+
+    if (result.data?.find((c) => c.id === CapabilityId.Pricing)) {
+      context.setCapabilities([{ id: CapabilityId.Pricing, required: true, revision: 1, dependencies: [], docs: null}])
+    }
 
     return this.capabilitiesScenarioHelper.validateCapabilities({
       result,
