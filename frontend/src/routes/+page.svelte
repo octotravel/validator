1bf707f9cba3e42e6bc1a80d6da81352dbdc45ef
Michal Panský <pansky.michal@gmail.com>
2024-02-16 13:41:44 +0100

    svelte init

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+
+	import { goto } from "$app/navigation";
+
+</script>
+<div class="container h-screen mx-auto flex justify-center items-center">
+    <div class="space-y-5 text-center">
+        <h1 class="h1">Octo Validator</h1>
+        <button type="button" class="btn variant-filled" on:click={() => goto('/reseller')}>Reseller</button>
+        <button type="button" class="btn variant-filled" on:click={() => goto('/supplier')}>Supplier</button>
+    </div>
+</div>
