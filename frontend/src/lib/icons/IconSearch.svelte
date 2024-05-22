1e3fa5f16fda8afd9f32c27639a5b21f2de872bc
Michal Panský <pansky.michal@gmail.com>
2024-05-22 15:48:47 +0200

    chore: Remove unused @tabler/icons-svelte package and update dependencies

--- frontend/src/lib/icons/IconSearch.svelte
+++ frontend/src/lib/icons/IconSearch.svelte
@@ -1,13 +1,26 @@
 <script>
-    export let size = '24'; // default size
-  </script>
+	export let size = '24'; // default size
+</script>
 
-<svg  xmlns="http://www.w3.org/2000/svg"  width={size}  height={size}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-search"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg>
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width={size}
+	height={size}
+	viewBox="0 0 24 24"
+	fill="none"
+	stroke="currentColor"
+	stroke-width="2"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	class="icon icon-tabler icons-tabler-outline icon-tabler-search"
+	><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
+		d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"
+	/><path d="M21 21l-6 -6" /></svg
+>
 
-  <style>
-    svg {
-      display: inline-block;
-      vertical-align: middle;
-    }
-  </style>
-  
+<style>
+	svg {
+		display: inline-block;
+		vertical-align: middle;
+	}
+</style>
