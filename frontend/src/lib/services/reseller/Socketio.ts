df48682b07d0f1664f3afa6559da6d66b9d0d592
Michal Panský <pansky.michal@gmail.com>
2024-06-17 11:06:12 +0200

    feat: Update Socketio to use PUBLIC_VALIDATOR_BASE_URL for socket connection

--- frontend/src/lib/services/reseller/Socketio.ts
+++ frontend/src/lib/services/reseller/Socketio.ts
@@ -1,8 +1,9 @@
+import { PUBLIC_VALIDATOR_BASE_URL } from '$env/static/public';
 import { io } from 'socket.io-client';
 
 export abstract class Socketio {
 	public static openSocket = (sessionId: string) => {
-		const socket = io('http://localhost:13000');
+		const socket = io(PUBLIC_VALIDATOR_BASE_URL);
 		socket.connect();
 		socket.emit('session', sessionId);
 		return socket;
