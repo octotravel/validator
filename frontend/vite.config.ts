04a07e91f98983924fecd569e427f59bf49c2b4c
Michal Panský <pansky.michal@gmail.com>
2025-06-20 16:18:20 +0200

    Refactor package.json and vite.config.ts for improved dependency management and plugin configuration. Reintroduced previously removed dependencies and streamlined Vite plugin setup.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,23 +4,30 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import 'dotenv/config';
 
-export default defineConfig({
-	resolve: { alias: { '@': '/src' } },
-	plugins: [
+const plugins = [
+	sveltekit(),
+	purgeCss({
+		safelist: {
+			// any selectors that begin with "hljs-" will not be purged
+			greedy: [/^hljs-/]
+		}
+	})
+];
+
+if (process.env.SENTRY_AUTH_TOKEN) {
+	plugins.unshift(
 		sentrySvelteKit({
 			sourceMapsUploadOptions: {
 				org: 'ventrata',
 				project: 'octo-validator-frontend-7n'
 			}
-		}),
-		sveltekit(),
-		purgeCss({
-			safelist: {
-				// any selectors that begin with "hljs-" will not be purged
-				greedy: [/^hljs-/]
-			}
 		})
-	],
+	);
+}
+
+export default defineConfig({
+	resolve: { alias: { '@': '/src' } },
+	plugins,
 	server: {
 		host: true,
 		port: Number(process.env.PUBLIC_APP_PORT) || 3000
